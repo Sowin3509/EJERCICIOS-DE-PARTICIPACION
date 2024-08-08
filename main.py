7e43593b6d9312282f8fe3da0276fa22b856f874
Sowin3509 <castillosowin1@gmail.com>
2024-08-08 06:50:41 -0500

    3

--- main.py
+++ main.py
@@ -19,3 +19,39 @@ class Punto:
 
     def calcular_distancia(self, otro_punto):
         return sqrt((self.x - otro_punto.x) ** 2 + (self.y - otro_punto.y) ** 2)
+
+class Rectangulo:
+    def __init__(self, punto1, punto2):
+        self.punto1 = punto1
+        self.punto2 = punto2
+
+    def calcular_perimetro(self):
+        ancho = abs(self.punto2.x - self.punto1.x)
+        alto = abs(self.punto2.y - self.punto1.y)
+        return 2 * (ancho + alto)
+
+    def calcular_area(self):
+        ancho = abs(self.punto2.x - self.punto1.x)
+        alto = abs(self.punto2.y - self.punto1.y)
+        return ancho * alto
+
+    def es_cuadrado(self):
+        ancho = abs(self.punto2.x - self.punto1.x)
+        alto = abs(self.punto2.y - self.punto1.y)
+        return ancho == alto
+
+from math import pi, sqrt
+
+class Circulo:
+    def __init__(self, centro, radio):
+        self.centro = centro
+        self.radio = radio
+
+    def calcular_area(self):
+        return pi * (self.radio ** 2)
+
+    def calcular_perimetro(self):
+        return 2 * pi * self.radio
+
+    def punto_pertenece(self, punto):
+        return self.centro.calcular_distancia(punto) <= self.radio
